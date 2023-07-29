e82011d1eff44fb5c98be0d112542402d9bbb699
Caleb Tseng <calebtseng07@gmail.com>
2023-07-29 05:16:39 -0700

    Write initial layout for about us page with minimal styling to get overlay img in place

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ import './AboutUs.css';
 const AboutUs = () => (
   <div className='rest__aboutus rest__bg flex__center section__padding' id="about">
     <div className="rest__aboutus-overlay flex__center">
-      <img src={images.G} alt='g letter' />
+      <img src={images.G} alt='g_overlay' />
     </div>
     <div className="rest__aboutus-content">
       <div className="rest__aboutus-content_about">
@@ -15,6 +15,14 @@ const AboutUs = () => (
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
         <button type='button' className='custom__button'>Know More</button>
       </div>
+    </div>
+    <div className="rest__aboutus-image">
+      <img src={images.knife} alt="knife img" />
+    </div>
+    <div className="rest__aboutus-content_history">
+      <SubHeading title="Our History" />
+      <p>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+      <button type='button' className='custom__button'>Know More</button>
     </div>
   </div>
 );
