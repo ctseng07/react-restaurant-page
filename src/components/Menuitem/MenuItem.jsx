9046d43052c9e135da1d8a2ff8e9fd308f39e062
Caleb Tseng <calebtseng07@gmail.com>
2023-08-03 04:03:05 -0700

    Add detail and design to menu items

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className='rest__menuItem'>
+    <div className="rest__menuItem-title">
+      <div className="rest__menuItem-name">
+        <p>{title}</p>
+      </div>
+      <div className="rest__menuItem-dash" />
+      <div className="rest__menuItem-price">
+        <p>{price}</p>
+      </div>
+    </div>
+    <div className="rest__menuItem-sub">
+      <p>{tags}</p>
+    </div>
   </div>
 );
 
