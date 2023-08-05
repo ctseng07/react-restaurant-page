0fe8dda5841a1c36351d86f71f7714c0e0cb88da
Caleb Tseng <calebtseng07@gmail.com>
2023-08-05 06:03:52 -0700

    Start writing and designing Laurel page

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
-const Laurels = () => (
-  <div>
-    Laurels
+
+const AwardInfo = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="rest__awardInfo">
+    <img src={imgUrl} alt="awards" />
+    <div className="rest__awardInfo-content">
+      <p className='rest__awardInfo-content_title'>{title}</p>
+      <p className='rest__awardInfo-content_text'>{subtitle}</p>
+    </div>
   </div>
+)
+
+// const Logo = () => (
+//   <div className="rest__laurels-logo">
+//     <img src={images.logo} alt="logo_img" />
+//   </div>
+// )
+
+const Laurels = () => (
+  <div className='rest__laurels rest__bg section__padding' id='awards'>
+    {/* <div className="rest__laurels-logo">
+      <img src={images.logo} alt="logo_img" />
+    </div> */}
+
+    <div className="rest__laurels-content">
+      <SubHeading title="Awards & Recognition" />
+      <h1 className='rest__laurels-content_title'>Our Laurels</h1>
+
+      <div className="rest__laurels-awards">
+        {data.awards.map((award) => <AwardInfo award={award} title={award.title} subtitle={award.subtitle} />)}
+      </div>
+    </div>
+
+    <div className="rest__laurels-image">
+      <img src={images.laurels} alt="laurels_img" />
+    </div>
+  </div >
 );
 
 export default Laurels;
