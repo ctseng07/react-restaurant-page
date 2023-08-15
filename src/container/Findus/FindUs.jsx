ce94329e18fa3bc5d9d59c2689d6c5a94e217b8f
Caleb Tseng <calebtseng07@gmail.com>
2023-08-15 04:26:16 -0700

    Create and finish designing the contact page

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import "./FindUs.css"
+import { SubHeading } from '../../components';
+import images from '../../constants/images';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='rest__findUs rest__bg section__padding' id='contact'>
+    <div className="rest__findUs-content">
+      <SubHeading title="Contact" />
+      <h1 className='rest__findUs-content_title'>Find Us</h1>
+      <div className="rest__findUs-content_info">
+        <p className='rest__findUs-content_text'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+        <p className='rest__findUs-content_text-title'>Opening Hours</p>
+        <p className="rest__findUs-content_text">Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className="rest__findUs-content_text">Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <button type='button' className='custom__button'>Vistit Us</button>
+    </div>
+    <div className="rest__findUs-image">
+      <img src={images.findus} alt='findus-img' />
+    </div>
   </div>
 );
 
